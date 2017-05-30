8f04181a9fff79e2418b425e38688e9f10d5aaad
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-05-30 14:03:37 +0100

    completed caesar shift

--- src/caesar-shift.js
+++ src/caesar-shift.js
@@ -1,14 +1,33 @@
 
 const ALPHA_SIZE = 26;
+const smallA = 'a'.charCodeAt(0);
+const capA   = 'A'.charCodeAt(0);
+const smallZ = 'z'.charCodeAt(0);
+const capZ   = 'Z'.charCodeAt(0);
 
 const encryptChar = (chr, shift) => {
-  const temp = chr.charCodeAt(0) + (shift % ALPHA_SIZE);
-  return temp > 122 ? String.fromCharCode(temp - ALPHA_SIZE) : String.fromCharCode(temp);
+  const charCode = chr.charCodeAt(0);
+  if (chr == '') return chr;
+  if ((charCode < smallA || charCode > smallZ) &&
+      (charCode < capA   || charCode > capZ)) return chr;
+  const temp = charCode + ((shift + ALPHA_SIZE) % ALPHA_SIZE);
+  return (temp > smallZ || (temp > capZ && temp < smallA)) ?
+           String.fromCharCode(temp - ALPHA_SIZE)
+           : String.fromCharCode(temp);
+}
+
+const shiftWord = (str, shift) => {
+  if (str === null) throw new Error('string cannot be null');
+  if (str === undefined) throw new Error('string cannot be undefined');
+  return [...str].map(x => encryptChar(x, shift)).join('');
 }
 
 const encrypt = (str, shift) => {
-  if (!str) return str;
-  return str.split('').map((x) => encryptChar(x, shift)).join('');
+  return shiftWord(str, shift);
 }
 
-export default encrypt;
+const decrypt = (str, shift) => {
+  return shiftWord(str, -shift);
+}
+
+export {encrypt, decrypt};
--- test/caesar-shift-test.js
+++ test/caesar-shift-test.js
@@ -1,7 +1,7 @@
 import {chai,
         expect,
        } from 'chai';
-import encrypt from '../src/caesar-shift.js';
+import {encrypt, decrypt} from '../src/caesar-shift.js';
 
 describe('caesar encrypt', () => {
    it('should encrypt an empty string with shift of 0', () => {
@@ -28,4 +28,52 @@ describe('caesar encrypt', () => {
    it('should encrypt more than one character', () => {
        expect(encrypt('ab', 1)).to.be.equal('bc');
    });
+   it('should encrypt more than one character', () => {
+       expect(encrypt('Z', 1)).to.be.equal('A');
+   });
+   it ('should throw an error with a null with msg', () => {
+       expect(() => encrypt(null, 0)).to.throw(Error, 'string cannot be null');
+   });
+   it ('should throw an error with a undefined with msg', () => {
+       expect(() => encrypt(undefined, 0)).to.throw(Error, 'string cannot be undefined');
+   });
+   it('should encrypt a single special character shift of 4', () => {
+      expect(encrypt('@', 4)).to.be.equal('@');
+   });
+});
+
+describe('caesar decrypt', () => {
+  it('should decrypt an empty string with shift of 0', () => {
+       expect(decrypt('', 0)).to.be.equal('');
+  });
+  it('should decrypt a single letter string with shift of 1', () => {
+       expect(decrypt('b', 1)).to.be.equal('a');
+  });
+  it('should decrypt a single letter string with shift of 0', () => {
+       expect(decrypt('c', 0)).to.be.equal('c');
+  });
+  it('should decrypt an a with shift of 1', () => {
+       expect(decrypt('a', 1)).to.be.equal('z');
+  });
+  it('should decrypt an a with shift of 4', () => {
+       expect(decrypt('a', 4)).to.be.equal('w');
+  });
+  it('should decrypt an cap A with shift of 4', () => {
+       expect(decrypt('A', 4)).to.be.equal('W');
+  });
+  it('should decrypt an cap A with shift of 26', () => {
+       expect(decrypt('A', 26)).to.be.equal('A');
+  });
+  it('should decrypt an cap A with shift of 52', () => {
+       expect(decrypt('A', 52)).to.be.equal('A');
+  });
+  it('should decrypt more than one character', () => {
+      expect(decrypt('ab', 1)).to.be.equal('za');
+  });
+  it ('should throw an error with a null with msg', () => {
+      expect(() => decrypt(null, 0)).to.throw(Error, 'string cannot be null');
+  });
+  it ('should throw an error with a undefined with msg', () => {
+      expect(() => encrypt(undefined, 0)).to.throw(Error, 'string cannot be undefined');
+  });
 });
